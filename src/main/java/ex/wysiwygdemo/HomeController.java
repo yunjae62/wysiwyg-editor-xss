7fc4fd399122ccf741d9c397dc430065912f16e3
yunjae62 <wodbs7758@naver.com>
2025-09-20 16:20:28 +0900

    feat: 게시글 앤드포인트 추가

--- src/main/java/ex/wysiwygdemo/HomeController.java
+++ src/main/java/ex/wysiwygdemo/HomeController.java
@@ -1,17 +1,35 @@
 package ex.wysiwygdemo;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final PostRepository postRepository;
 
     @GetMapping("")
     public String home(Model model) {
-        model.addAttribute("message", "Hello World!!!!");
+        List<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "home";
+    }
+
+    @GetMapping("/posts/new")
+    public String newPost() {
+        return "newPost";
+    }
+
+    @PostMapping("/posts/new")
+    public String newPost(Post post) {
+        postRepository.save(post);
+        return "redirect:/";
     }
 }
