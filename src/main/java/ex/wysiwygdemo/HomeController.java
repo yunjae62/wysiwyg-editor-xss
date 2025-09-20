bc1aaa97c4c8a36603226d310cc3ce6b8e394def
yunjae62 <wodbs7758@naver.com>
2025-09-20 15:41:03 +0900

    feat: 메인 페이지 추가

---
+++ src/main/java/ex/wysiwygdemo/HomeController.java
@@ -0,0 +1,17 @@
+package ex.wysiwygdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello World!!!!");
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<body>
+<h1>${message}</h1>
+</body>
+</html>
