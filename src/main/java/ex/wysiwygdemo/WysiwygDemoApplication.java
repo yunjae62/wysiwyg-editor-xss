f2f6f0c903128d2b4d1cd35eb3afb8da11aeb64b
yunjae62 <wodbs7758@naver.com>
2025-09-20 15:30:25 +0900

    first commit

---
+++ src/main/java/ex/wysiwygdemo/WysiwygDemoApplication.java
@@ -0,0 +1,12 @@
+package ex.wysiwygdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WysiwygDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WysiwygDemoApplication.class, args);
+    }
+}
